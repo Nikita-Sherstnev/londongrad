ff3f02707244e6b730fbe9b8f3e6f4f6a6b09de1
Nikita-Sherstnev <nik124854@gmail.com>
2022-08-24 23:46:42 +0400

    add repr to funcs, update example

--- examples/graph.py
+++ examples/graph.py
@@ -3,11 +3,15 @@ from londongrad.utils import draw_graph
 
 
 if __name__ == '__main__':
-    x = lg.tensor([2.0])
-    a = x ** 2
-    y = ((-a + x) ** 3) * 6.0
+    def goldstein(x, y):
+        z = (1 + (x + y + 1)**2 * (19 - 14*x + 3*x**2 - 14*y + 6*x*y + 3*y**2)) * \
+            (30 + (2*x - 3*y)**2 * (18 - 32*x + 12*x**2 + 48*y - 36*x*y + 27*y**2))
+        return z
 
-    y.backward()
+    x = lg.tensor([1.0])
+    y = lg.tensor([1.0])
+    z = goldstein(x, y)
+    z.backward()
 
-    dot = draw_graph(y, format='png')
+    dot = draw_graph(z, format='png')
     dot.render('graph')
